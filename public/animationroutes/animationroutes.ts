5f4f3e8928e406606b3ac19f68b2eb2c3f6a259f
infuman69 <saharoy.soham123@gmail.com>
2023-09-24 23:28:42 +0530

    create animation routes

---
+++ public/animationroutes/animationroutes.ts
@@ -0,0 +1,7 @@
+const animationRoutes = [
+    'fade',
+    'rotate',
+    'scale',
+]
+
+export default animationRoutes
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,75 +1,42 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import animationRoutes from "../../public/animationroutes/animationroutes";
 
 export default function Home() {
   const [word, setWord] = useState("");
 
   const router = useRouter();
 
-  const [animationType, setAnimationType] = useState("swipe");
+  // const [animationType, setAnimationType] = useState("swipe");
 
-  const handleAnimate = () => {
+  const handleAnimate = (animationType: string) => {
     router.push(`/${animationType}`);
   };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="mb-4">
-        <label htmlFor="word" className="block text-gray-700 font-bold mb-2">
-          Word:
-        </label>
-        <input
-          type="text"
-          id="word"
-          name="word"
-          value={word}
-          onChange={() =>{
-            setWord(word);
-          }}
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        />
+        <h1>Animation Menu</h1>
       </div>
       <div className="mb-4">
         <span className="block text-gray-700 font-bold mb-2">
           Animation Type:
         </span>
-        <div className="flex items-center">
-          <label htmlFor="swipe" className="mr-4">
-            <input
-              type="radio"
-              id="swipe"
-              name="animationType"
-              value="swipe"
-              checked={animationType === "swipe"}
-              onChange={() => {
-                setAnimationType("swipe");
-              }}
-              className="mr-2"
-            />
-            Swipe
-          </label>
-          <label htmlFor="geometric">
-            <input
-              type="radio"
-              id="geometric"
-              name="animationType"
-              value="geometric"
-              checked={animationType === "geometric"}
-              onChange={() => {
-                setAnimationType("geometric");
-              }}
-              className="mr-2"
-            />
-            Geometric
-          </label>
+        <div className="flex items-center gap-6">
+          {animationRoutes.map((route, index) => {
+            return (
+              <button
+                key={index}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => handleAnimate(route)}
+              >
+                {route}
+              </button>
+            );
+          })}
         </div>
       </div>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      onClick={handleAnimate}
-      >
-        Animate
-      </button>
     </div>
   );
 }
